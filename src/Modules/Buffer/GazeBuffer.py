a607282a891201a9e4b86d8107ede6d8704b596e
SanghwaLee <devlsh0124@gmail.com>
2022-05-31 13:01:41 +0900

    feat: Add shift function in Gaze Buffer

--- src/Modules/Buffer/GazeBuffer.py
+++ src/Modules/Buffer/GazeBuffer.py
@@ -1,57 +1,52 @@
-import math
+import numpy as np
+
+from .base import BaseBuffer
 
 
-class BaseBuffer:
-    name = "base"
-    code = "-1"
-    down_p = 1
-    up_p = 1
-    type = -1
-    delay = 0
-    buffer = 1
-    last_glance = True
+class GazeBuffer(BaseBuffer):
+    inc_coeff = 0
+    dec_coeff = 0
+    inc_rate = 0
+    dec_rate = 0
+    last = {
+        'on': 0,
+        'off': 0
+    }
 
-    def update(self, gaze, time):
-        # 시선이 해당 영역에 있는 경우
-        if gaze == self.code:
-            # 이전 시선 역시 이 Buffer인 경우
-            if self.last_glance:
-                # Buffer가 최대치인 경우
-                if self.buffer >= 1:
-                    self.stay()
-                # 최대치가 아닌 경우
-                else:
-                    self.up(time)
-            # 이전 시선이 Buffer가 아닌 경우
-            else:
-                # Delay가 0.1초가 되었는지 확인
-                if self.delay >= 0.1:
-                    self.last_glance = True
-                    # Buffer가 최대인 경우
-                    if self.buffer >= 1:
-                        self.stay()
-                    else:
-                        self.up(time)
-                # Delay가 0.1초가 안 되는 경우
-                else:
-                    self.delay += time
-                    self.stay()
-        # 시선이 해당 영역에 없는 경우
+    def update(self, time, **kwargs):
+        pass
+
+    def shift_on(self, shift, frame_time, v, s):
+        """
+        shift:
+        [ 0 ] : gaze != target
+        [ 1 ] : 시선이 현재 버퍼로 이동
+        [ 2 ] : 버퍼에 시선이 존재
+        """
+        if shift == 0:
+            result = 0
+        elif shift == 1:
+            result = -1 * (np.log(1 - self.buffer)) / self.inc_coeff
         else:
-            self.down(gaze, time)
-            if self.buffer < 0:
-                self.buffer = 0
+            result = self.last['on'] + frame_time * (self.weight(v, s) ** -1)
 
-    def down(self, gaze, time):
-        self.buffer -= math.exp(-1 * self.buffer) * time * self.down_p
-        self.last_glance = False
-        self.delay = 0
+        self.last['on'] = result
+        return result
 
-    def up(self, time):
-        self.buffer += math.exp(-1 * self.buffer) * time * self.up_p
-        if self.buffer >= 1:
-            self.buffer = 1
-        self.last_glance = True
+    def shift_off(self, shift, frame_time, v, s):
+        """
+        shift:
+        [ 0 ] : 버퍼에 시선이 존재
+        [ 1 ] : 시선이 현재 버퍼에서 벗어남
+        [ 2 ] : 버퍼에 시선이 존재하지 않음
+        """
+        if shift == 0:
+            result = 0
+        elif shift == 1:
+            result = np.log(1 + np.exp(-1 * self.dec_coeff / self.dec_rate) - self.buffer) / self.dec_coeff
+            result -= 1 / self.dec_rate
+        else:
+            result = self.last['off'] + frame_time * self.weight(v, s)
 
-    def stay(self):
-        self.last_glance = True
+        self.last['off'] = result
+        return result
